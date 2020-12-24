b79745d1df2628a85d99a5ca7d07e80523c07e33
lolthoi <bayern.munich.96@gmail.com>
2020-12-24 11:47:57 +0700

    second fix after check

--- assets/js/style.js
+++ assets/js/style.js
@@ -7,6 +7,29 @@ $(document).ready(function () {
             $(this).parent().find('.contact__info').toggleClass('d-block');
         });
     });
+    var sec = $('.sec').html();
+    var min = $('.min').html();
+    var hour = $('.hour').html();
+    var day = $('.day').html();
+    setInterval(function () {
+        sec -= 1;
+        if (sec == -1) {
+            sec = 59;
+            min -= 1;
+            if (min == -1) {
+                min = 59;
+                hour -= 1;
+                if (hour == -1) {
+                    hour = 23;
+                    day -= 1;
+                }
+            }
+        }
+        $('.sec').html(sec);
+        $('.min').html(min);
+        $('.hour').html(hour);
+        $('.day').html(day);
+    }, 1000);
 
     $('.category__icon').click(function () {
         $('.category__menu').slideToggle();
@@ -19,18 +42,23 @@ $(document).ready(function () {
         infinite: true,
         rows: 2,
         slidesToShow: 4,
-        variableWidth: true,
         responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
                 }
             }
         ]
@@ -39,20 +67,17 @@ $(document).ready(function () {
         dots: false,
         infinite: true,
         slidesToShow: 4,
-        variableWidth: true,
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 992,
                 settings: {
                     slidesToShow: 2,
-                    variableWidth: true,
                 }
             },
             {
                 breakpoint: 576,
                 settings: {
-                    slidesToShow: 1,
-                    variableWidth: true,
+                    slidesToShow: 1
                 }
             }
         ]
