12da3f1af07959048a172a6726593bdfd390d75c
Dương Hoàng <manchestervn1996@gmail.com>
2021-05-11 16:49:37 +0700

    add day.cs

---
+++ Day.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace fitness.Models
+{
+    public partial class Day
+    {
+        public Day()
+        {
+            Schedule = new HashSet<Schedule>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public virtual ICollection<Schedule> Schedule { get; set; }
+    }
+}
